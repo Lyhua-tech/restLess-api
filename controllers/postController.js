d4f27514c153664fbe20575b8d3f88f0c1f60fd9
Lyhua-tech <slyhour69@gmail.com>
2024-08-28 22:24:37 +0700

    complete rest api

--- controllers/postController.js
+++ controllers/postController.js
@@ -37,3 +37,47 @@ exports.getAllPost = async (req, res, next) => {
     });
   }
 };
+exports.deleteUser = async(req, res, next) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params._id)
+    if (!post){
+      res.status(404).json({
+        status: 'fail',
+        message: 'this post is not found!'
+      })
+    }
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    })
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error.message,
+    })
+  }
+}
+
+exports.updateUser = async(req, res, next) => {
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!updatedPost){
+      res.status(404).json({
+        status: 'fail',
+        message: 'this post is not found!'
+      })
+    }
+    res.status(202).json({
+      status: 'success',
+      post: updatedPost,
+    })
+  }catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error.message,
+    });
+  }
+}
